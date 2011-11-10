17e45d05cef48c319a0810ca9afc9e78bd88813d
S <s>
2011-11-09 20:49:54 -0500

    {"type":"TI", "runs":"2"}

--- Breakout.java
+++ Breakout.java
@@ -75,7 +75,7 @@ public class Breakout extends GraphicsProgram {
     private static final double VMIN = 1.0;
 
 /** Animation cycle delay */
-    private static final int DELAY = 10;
+    private static final int DELAY = 9;
     
 /** Paddle edge width percentage */    
     private static final double PEDGE = 0.15;
