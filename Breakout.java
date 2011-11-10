90ef8afefa1e1e8b7baec4dbe09f7848c1011434
S <s>
2011-11-09 21:12:10 -0500

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -274,7 +274,7 @@ public class Breakout extends GraphicsProgram {
     		    		
     		// Assume that the ball will move an equal amount 
     		// in the same direction as it would have otherwise
-    		//ball.move(2*diff, 0);
+    		ball.move(2*diff, 0);
     		//waitForClick();
     	} 
     	else if(vx<0 && leftBallX>rightPaddleX){
@@ -285,7 +285,7 @@ public class Breakout extends GraphicsProgram {
     		
     		// Assume that the ball will move an equal amount 
     		// in the same direction as it would have otherwise
-    		//ball.move(2*diff, 0);
+    		ball.move(2*diff, 0);
     		//waitForClick();
     	}    	
     	
